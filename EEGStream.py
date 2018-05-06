3272e81175f1d44006445e45084b6a801fcff776
Sean Pollock <seanpollock3344@gmail.com>
2018-05-06 19:10:31 -0400

    reading .mat files from dataset

--- EEGStream.py
+++ EEGStream.py
@@ -1,13 +1,26 @@
 import os
+import scipy.io as sio
 
-data_dir = './BCICIV_1calib_1000Hz_asc/'
+data_dir = './eeg_data'
 
-def get_files(path):
+def get_files(dir_path):
     try:
-        return os.listdir(path)
+        return os.listdir(dir_path)
     except ValueError:
-        print('{0} does not exist'.format(path))
+        print('{0} does not exist'.format(dir_path))
+
+def read_file(file_path):
+    try:
+        data = sio.loadmat(file_path)
+        print('data', data['data'])
+        sampling_rate = data['data'][0][0][0][0][3][0][0]
+        print('sampling_rate', sampling_rate)
+
+    except ValueError:
+        print('could not read data file {0}'.format(file_path))
+
 
 if __name__ == '__main__':
     files = get_files(data_dir)
-    print('files', files)
+    file_path = '{0}/{1}'.format(data_dir, files[0])
+    read_file(file_path)
