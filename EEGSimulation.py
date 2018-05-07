3c496dea70364c7d24a36513c9a9f54351f731d6
Sean Pollock <seanpollock3344@gmail.com>
2018-05-07 00:24:11 -0400

    small fixes. successfully parsing training data

--- EEGSimulation.py
+++ EEGSimulation.py
@@ -13,36 +13,33 @@ class EEGSimulation():
         except ValueError:
             print('{0} does not exist'.format(dir_path))
 
-    def read_file(file_path):
+    def read_file(self, file_path):
         try:
             self.data = sio.loadmat(file_path)
-            self.sampling_rate = data['data'][0][0][0][0][3][0][0]
-            print('sampling_rate', sampling_rate)
-
+            self.sampling_rate = self.data['data'][0][0][0][0][3][0][0]
+            self.trial_length = 5 * self.sampling_rate
         except ValueError:
              print('could not read data file {0}'.format(file_path))
-    
+
     def start(self):
         files = self.get_files(self.data_dir)
-        file_path = '{0}/{1}'.format(data_dir, files[0])
+        file_path = '{0}/{1}'.format(self.data_dir, files[0])
         self.read_file(file_path)
         self.run_simulation()
 
     def run_simulation(self):
         for run in range(len(self.data['data'][0])):
-            y.append(data['data'][0][run][0][0][2][0])
-            timestamps = data['data'][0][run][0][0][1][0]
-            raw_data = data['data'][0][run][0][0][0].transpose()
+            self.y.append(self.data['data'][0][run][0][0][2][0])
+            timestamps = self.data['data'][0][run][0][0][1][0]
+            raw_data = self.data['data'][0][run][0][0][0].transpose()
 
             for start in timestamps:
-                end = start + self.trial_length 
+                end = start + self.trial_length
                 self.x.append(raw_data[:, start:end])
 
-        del raw_data
-        del data
-
+        print('self.x', self.x)
+        print('self.y', self.y)
 
 if __name__ == '__main__':
-    files = get_files(data_dir)
-    data = read_file(file_path)
-    
+    simulation = EEGSimulation()
+    simulation.start()
