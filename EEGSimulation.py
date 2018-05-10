753e34cd38a04ed3e7af0261d5f0122f4ab1b959
Sean Pollock <seanpollock3344@gmail.com>
2018-05-10 19:03:04 -0400

    small fixes to data parsing

--- EEGSimulation.py
+++ EEGSimulation.py
@@ -6,6 +6,8 @@ class EEGSimulation():
         self.data_dir = './eeg_data'
         self.x = []
         self.y = []
+        self.y_dim = 2
+        self.x_dim = (15, 2560)
 
     def get_files(self, dir_path):
         try:
@@ -18,7 +20,7 @@ class EEGSimulation():
             self.data = sio.loadmat(file_path)
             self.sampling_rate = self.data['data'][0][0][0][0][3][0][0]
             self.trial_length = 5 * self.sampling_rate
-            print('sampling_rate', self.sampling_rate)
+            print('self.trial_length', self.trial_length)
 
         except ValueError:
              print('could not read data file {0}'.format(file_path))
@@ -31,6 +33,7 @@ class EEGSimulation():
 
     def run_simulation(self):
         for run in range(len(self.data['data'][0])):
+            print('run', run)
             self.y.append(self.data['data'][0][run][0][0][2][0])
             timestamps = self.data['data'][0][run][0][0][1][0]
             raw_data = self.data['data'][0][run][0][0][0].transpose()
@@ -39,8 +42,9 @@ class EEGSimulation():
                 end = start + self.trial_length
                 self.x.append(raw_data[:, start:end])
 
-        del raw_data
-        del data
+        # print('self.y', self.y, len(self.y), self.y[0].shape)
+        # print('self.x', self.x, len(self.x))
+        print('sample', type(self.x[0]), self.x[0].shape)
 
 
 if __name__ == '__main__':
