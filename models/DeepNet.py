1345d5e320e3dd9cd3f201cd0894d74983454fbf
Sean Pollock <seanpollock3344@gmail.com>
2018-05-10 16:49:51 -0400

    porting over deep net with mini_batch

---
+++ models/DeepNet.py
@@ -0,0 +1,59 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
+import random
+from time import time
+import numpy as np
+
+class DeepNet():
+    def __init__(self, hyper_params):
+        self.init_hyper_params(hyper_params)
+        self.memory = []
+
+    def init_hyper_params(self, params):
+        self.alpha = params['alpha']
+        self.alpha_decay = params['alpha_decay']
+        self.epsilon = params['epsilon']
+        self.epislon_min = params['epsilon_min']
+        self.epsilon_decay = params['epsilon_decay']
+        self.batch_size = params['batch_size']
+        self.gamma = params['gamma']
+
+    def build_network(self, state_dim, action_dim):
+        model = Sequential()
+        model.add(Dense(512, activation='relu', input_dim=state_dim))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(512, activation='relu'))
+        model.add(Dense(action_dim, activation='linear'))
+        model.compile(optimizer=Adam(lr=self.alpha, decay=self.alpha_decay), loss='mse')
+        self.model = model
+
+        self.tensorboard = TensorBoard(
+                log_dir="logs/{}".format(time()),
+                histogram_freq=0,
+                batch_size=32,
+                write_graph=True)
+
+    def calc_epsilon(self):
+        epsilon = self.epsilon * self.epsilon_decay
+        self.epsilon = max(self.epislon_min, epsilon)
+
+    def update_network(self):
+        x_batch, y_batch = [], []
+        mini_batch = random.sample(self.memory, min(len(self.memory), self.batch_size))
+
+        for old_state, action, reward, new_state, done in mini_batch:
+            y_target = self.model.predict(old_state)
+            if done:
+                # estimated future reward is 0
+                y_target[0][action] = reward
+            else:
+                # find estimated future rewards
+                y_target[0][action] = reward + self.gamma * np.max(self.model.predict(new_state)[0])
+
+            x_batch.append(old_state[0])
+            y_batch.append(y_target[0])
+
+        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), callbacks=[self.tensorboard])
